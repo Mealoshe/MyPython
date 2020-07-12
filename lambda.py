56ee8f160814911c5e600c6367d68f533a81ac49
unknown <mealoshe@gmail.com>
2020-07-11 19:31:26 -0700

    First commit

---
+++ lambda.py
@@ -0,0 +1,15 @@
+
+# lambda.py
+
+people = [
+  {"name": "Harry", "house": "Gryffindor"},
+  {"name": "Cho", "house": "Ravenclaw"},
+  {"name": "Draco", "house": "Slytherin"}
+
+]
+
+
+
+people.sort(key=lambda person: person["name"])
+
+print(people)
